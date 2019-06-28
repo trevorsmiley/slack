9e739c7cc7a9101ffc97b743b9882bfdbb9f8e7e
Trevor Smiley <trevor.smiley@gmail.com>
2019-06-27 22:54:26 -0400

    Emoji upload support

--- emoji.go
+++ emoji.go
@@ -33,3 +33,30 @@ func (api *Client) GetEmojiContext(ctx context.Context) (map[string]string, erro
 
 	return response.Emoji, nil
 }
+
+// AddEmoji uploads an emoji
+func (api *Client) AddEmoji(image, name string) error {
+	return api.AddEmojiContext(context.Background(), image, name)
+}
+
+// AddEmojiContext uploads an emoji with a custom context
+func (api *Client) AddEmojiContext(ctx context.Context, image, name string) error {
+	values := url.Values{
+		"token": {api.token},
+		"mode":  {"data"},
+		"name":  {name},
+	}
+	response := &emojiResponseFull{}
+
+	err := postLocalWithMultipartResponse(ctx, api.httpclient, "emoji.add", image, "image", values, response, api)
+
+	if err != nil {
+		return err
+	}
+
+	if response.Err() != nil {
+		return response.Err()
+	}
+
+	return nil
+}
